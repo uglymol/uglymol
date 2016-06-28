5cd337cebb0964d731e9862e9a59d0fd03ca1c30
Marcin Wojdyr <wojdyr@gmail.com>
2016-06-28 01:01:32 +0100

    concat.sh

---
+++ concat.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+cat > uglymol.js << EOF
+/* UglyMol - macromolecular viewer for crystallographers, a fork of xtal.js.
+ * https://uglymol.github.io
+
+$(cat LICENSE)
+*/
+
+var UGLYMOL_VERSION = '$(node -p "require('./package.json').version")'
+var THREE = THREE || require('three'); // eslint-disable-line
+EOF
+
+cat src/unitcell.js \
+    src/model.js \
+    src/elmap.js \
+    src/isosurface.js \
+    src/viewer.js \
+  | grep -v '|| require(' \
+  | grep -v 'if (typeof module !== ' \
+  >> uglymol.js
+
+cat >> uglymol.js << EOF
+if (typeof module !== 'undefined') module.exports = {
+  VERSION: UGLYMOL_VERSION,
+  UnitCell: UnitCell,
+  Model: Model,
+  ElMap: ElMap,
+  isosurface: isosurface,
+  Viewer: Viewer
+};
+EOF
+
+wc uglymol.js
