7e6e6435063936bda5e2e004dde8060eb9317f12
Marcin Wojdyr <wojdyr@gmail.com>
2016-07-03 20:39:48 +0100

    updated eslint

--- perf/modulo.js
+++ perf/modulo.js
@@ -6,62 +6,63 @@ var suite = new Benchmark.Suite();
 var VALUES = [21.2, 30.5, 25.9, -7.2, 0.4, -0.4];
 //var VALUES = [21, 30, 26, -7, 0, -1];
 var M = 3;
-var result1, result2, result3, result4, result5;
+var res = [0, 0];
 
 // like Math.euclideanModulo() from three.js
 suite.add('%,+,%', function () {
-  result1 = 0;
+  res[0] = 0;
   for (var i = 0; i < VALUES.length; i++) {
     var n = VALUES[i];
-    result1 += ((n % M) + M) % M;
+    res[0] += ((n % M) + M) % M;
   }
 });
 
 suite.add('%,if,+=', function () {
-  result2 = 0;
+  res[1] = 0;
   for (var i = 0; i < VALUES.length; i++) {
     var n = VALUES[i];
     var x = n % M;
     if (x < 0) {
       x += M;
     }
-    result2 += x;
+    res[1] += x;
   }
 });
 
 suite.on('cycle', function (event) { console.log(' ' + event.target); });
 suite.run();
-console.log('  results: ' + result1 + ' and ' + result2);
+console.log('  results: ' + res.join(', '));
 
 
 var NUMBERS = ['   2', ' 131', '  88', '3032', ' 912', '6312', '8194', '6518'];
 suite = new Benchmark.Suite();
+res = [0, 0, 0, 0, 0];
 suite.add('Number', function () {
-  result1 = 0;
-  for (var i = 0; i < NUMBERS.length; i++) result1 += Number(NUMBERS[i]);
+  res[0] = 0;
+  for (var i = 0; i < NUMBERS.length; i++) res[0] += Number(NUMBERS[i]);
 });
 
 suite.add('|0', function () {
-  result2 = 0;
-  for (var i = 0; i < NUMBERS.length; i++) result2 += NUMBERS[i] | 0;
+  res[1] = 0;
+  for (var i = 0; i < NUMBERS.length; i++) res[1] += NUMBERS[i] | 0;
 });
 
 suite.add('Number|0', function () {
-  result3 = 0;
-  for (var i = 0; i < NUMBERS.length; i++) result3 += Number(NUMBERS[i]) | 0;
+  res[2] = 0;
+  for (var i = 0; i < NUMBERS.length; i++) res[2] += Number(NUMBERS[i]) | 0;
 });
 
 suite.add('parseInt()', function () {
-  result4 = 0;
+  res[3] = 0;
   // eslint-disable-next-line radix
-  for (var i = 0; i < NUMBERS.length; i++) result4 += parseInt(NUMBERS[i]);
+  for (var i = 0; i < NUMBERS.length; i++) res[3] += parseInt(NUMBERS[i]);
 });
 
 suite.add('parseInt(,10)', function () {
-  result5 = 0;
-  for (var i = 0; i < NUMBERS.length; i++) result5 += parseInt(NUMBERS[i], 10);
+  res[4] = 0;
+  for (var i = 0; i < NUMBERS.length; i++) res[4] += parseInt(NUMBERS[i], 10);
 });
 
 suite.on('cycle', function (event) { console.log(' ' + event.target); });
 suite.run();
-console.log('  results: ' + result1 + ', ' + result2 + ', ' + result3);
+console.log('  results: ' + res.join(', '));
