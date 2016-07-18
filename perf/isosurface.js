ecbc9131e19c97c1de0136dcfcb6258cb8333519
Marcin Wojdyr <wojdyr@gmail.com>
2016-07-18 16:42:23 +0100

    update eslint, outerIIFEBody works as expected now

--- perf/isosurface.js
+++ perf/isosurface.js
@@ -1,17 +1,18 @@
-//'use strict';
 
 var ElMap = ElMap || require('../src/elmap'); // eslint-disable-line
 var isosurface = isosurface || require('../src/isosurface'); // eslint-disable-line
 var util = util || require('./util'); // eslint-disable-line
 
-(function () {
-  var dmap_buf = util.open_as_array_buffer('1mru.omap');
-  var map = new ElMap();
-  map.from_dsn6(dmap_buf.slice(0));
-  map.extract_block(15, [25, 26, 35]);
+(function () {  // namespace is needed for perf.html
+'use strict';
 
-  util.bench('isosurface', function () {
-    var bl = map.block;
-    isosurface(bl.points, bl.values, bl.size, 1.0);
-  });
+var dmap_buf = util.open_as_array_buffer('1mru.omap');
+var map = new ElMap();
+map.from_dsn6(dmap_buf.slice(0));
+map.extract_block(15, [25, 26, 35]);
+
+util.bench('isosurface', function () {
+  var bl = map.block;
+  isosurface(bl.points, bl.values, bl.size, 1.0);
+});
 })();
--- perf/viewer.js
+++ perf/viewer.js
@@ -1,41 +1,42 @@
-'use strict';
 
 var util = util || require('./util'); // eslint-disable-line
 var Model = Model || require('../src/model'); // eslint-disable-line
 var ElMap = ElMap || require('../src/elmap'); // eslint-disable-line
 var Viewer = Viewer || require('../src/viewer'); // eslint-disable-line
 
-(function () {
-  var pdb_string = util.open_as_utf8('1mru.pdb');
-  var cmap_buf = util.open_as_array_buffer('1mru.map');
+(function () {  // namespace is needed for perf.html
+'use strict';
 
-  var viewer = new Viewer();
-  var model = new Model();
-  model.from_pdb(pdb_string);
-  viewer.set_model(model);
-  var emap = new ElMap();
-  emap.from_ccp4(cmap_buf);
+var pdb_string = util.open_as_utf8('1mru.pdb');
+var cmap_buf = util.open_as_array_buffer('1mru.map');
 
-  util.bench('add trace', function () {
-    viewer.config.render_style = 'trace';
-    viewer.set_atomic_objects(viewer.model_bags[0]);
-    viewer.clear_atomic_objects(viewer.model_bags[0]);
-  });
+var viewer = new Viewer();
+var model = new Model();
+model.from_pdb(pdb_string);
+viewer.set_model(model);
+var emap = new ElMap();
+emap.from_ccp4(cmap_buf);
 
-  util.bench('add bonds', function () {
-    viewer.config.render_style = 'lines';
-    viewer.set_atomic_objects(viewer.model_bags[0]);
-    viewer.clear_atomic_objects(viewer.model_bags[0]);
-  });
+util.bench('add trace', function () {
+  viewer.config.render_style = 'trace';
+  viewer.set_atomic_objects(viewer.model_bags[0]);
+  viewer.clear_atomic_objects(viewer.model_bags[0]);
+});
 
-  util.bench('add ribbon', function () {
-    viewer.config.render_style = 'ribbon';
-    viewer.set_atomic_objects(viewer.model_bags[0]);
-    viewer.clear_atomic_objects(viewer.model_bags[0]);
-  });
+util.bench('add bonds', function () {
+  viewer.config.render_style = 'lines';
+  viewer.set_atomic_objects(viewer.model_bags[0]);
+  viewer.clear_atomic_objects(viewer.model_bags[0]);
+});
 
-  util.bench('add_map+clear', function () {
-    viewer.add_map(emap, false);
-    viewer.clear_el_objects(viewer.map_bags.pop());
-  });
+util.bench('add ribbon', function () {
+  viewer.config.render_style = 'ribbon';
+  viewer.set_atomic_objects(viewer.model_bags[0]);
+  viewer.clear_atomic_objects(viewer.model_bags[0]);
+});
+
+util.bench('add_map+clear', function () {
+  viewer.add_map(emap, false);
+  viewer.clear_el_objects(viewer.map_bags.pop());
+});
 })();
