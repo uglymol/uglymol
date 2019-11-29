86238c8bf675718b978fbbc75e5e6ef848f93111
Marcin Wojdyr <wojdyr@gmail.com>
2019-11-29 17:21:55 +0100

    more work on mtz.js

--- uglymol.js
+++ uglymol.js
@@ -8510,45 +8510,32 @@ ReciprocalViewer.prototype.MOUSE_HELP =
 
 /*::
  import type {Viewer} from './viewer.js'
- declare var Module: any;
- declare var HEAPF32: Float32Array;
  */
 
-function add_map_from_mtz(viewer, mtz, map_data, is_diff/*:boolean*/) {
-  //t2.push(performance.now());
-  var map = new ElMap();
-  map.unit_cell = new UnitCell(
-    mtz.cell_param(0), mtz.cell_param(1), mtz.cell_param(2),
-    mtz.cell_param(3), mtz.cell_param(4), mtz.cell_param(5));
-  map.stats.rms = mtz.rmsd;
-  var len = mtz.nx * mtz.ny * mtz.nz;
-  console.log('fft size', mtz.nx, mtz.ny, mtz.nz);
-  map.grid = new GridArray([mtz.nz, mtz.ny, mtz.nx]);
-  map.grid.values.set(HEAPF32.subarray(map_data/4, map_data/4 + len));
-  viewer.add_map(map, is_diff);
-  //t3.push(performance.now());
+function log_timing(t0/*:number*/, text/*:string*/) {
+  console.log(text + ': ' + (performance.now() - t0).toFixed(2) + ' ms.');
 }
 
-function load_maps_from_mtz_buffer(viewer/*:Viewer*/, mtz_buf/*:ArrayBuffer*/,
+function add_map_from_mtz(viewer, mtz, map_data, is_diff/*:boolean*/) {
+  var map = new ElMap();
+  var mc = mtz.cell;
+  map.unit_cell = new UnitCell(mc.a, mc.b, mc.c, mc.alpha, mc.beta, mc.gamma);
+  map.stats.rms = mtz.rmsd;
+  map.grid = new GridArray([mtz.nx, mtz.ny, mtz.nz]);
+  map.grid.values.set(map_data);
+  viewer.add_map(map, is_diff);
+}
+
+function load_maps_from_mtz_buffer(viewer/*:Viewer*/, mtz/*:Object*/,
                                    labels/*:?string[]*/) {
-  //let t0 = performance.now();
-  /* global Module, HEAPF32 */
-  var arr = new Uint8Array(mtz_buf);
-  var buffer = Module._malloc(arr.length);
-  Module.writeArrayToMemory(arr, buffer);
-  var mtz = new Module.Mtz;
-  if (!mtz.read(buffer, arr.length)) {
-    viewer.hud(mtz.last_error, 'ERR');
-    return;
-  }
-  //let t1 = performance.now();
-  //let t2 = [];
-  //let t3 = [];
   if (labels != null) {
     for (var n = 0; n < labels.length; n += 2) {
       if (labels[n] === '') { continue; }
+      var t0 = performance.now();
       var map_data = mtz.calculate_map_from_labels(labels[n], labels[n+1]);
-      if (map_data === 0) {
+      log_timing(t0, 'map ' + mtz.nx + 'x' + mtz.ny + 'x' + mtz.nz +
+                     ' calculated in');
+      if (map_data == null) {
         viewer.hud(mtz.last_error, 'ERR');
         continue;
       }
@@ -8558,39 +8545,40 @@ function load_maps_from_mtz_buffer(viewer/*:Viewer*/, mtz_buf/*:ArrayBuffer*/,
   } else {  // use default labels
     for (var nmap = 0; nmap < 2; ++nmap) {
       var is_diff$1 = (nmap == 1);
+      var t0$1 = performance.now();
       var map_data$1 = mtz.calculate_map(is_diff$1);
-      if (map_data$1 !== 0) {
+      log_timing(t0$1, 'map ' + mtz.nx + 'x' + mtz.ny + 'x' + mtz.nz +
+                     ' calculated in');
+      if (map_data$1 != null) {
         add_map_from_mtz(viewer, mtz, map_data$1, is_diff$1);
       }
     }
   }
-  //Module._free(buffer);
   mtz.delete();
-  //let t4 = performance.now();
-  //console.log('reading mtz: ' + (t1 - t0) + ' ms.');
-  //console.log('map 1 fft: ' + (t2[0] - t1) + ' ms.');
-  //console.log('map 1 copy: ' + (t3[0] - t2[0]) + ' ms.');
-  //console.log('map 2 fft: ' + (t2[1] - t3[0]) + ' ms.');
-  //console.log('map 2 copy: ' + (t3[1] - t2[1]) + ' ms.');
-  //console.log('total: ' + (t4 - t0) + ' ms.');
 }
 
-function load_maps_from_mtz(viewer/*:Viewer*/, url/*:string*/,
-                            labels/*:?string[]*/,
-                            callback/*:?Function*/) {
+function load_maps_from_mtz(Gemmi/*:Object*/, viewer/*:Viewer*/, url/*:string*/,
+                            labels/*:?string[]*/, callback/*:?Function*/) {
   viewer.load_file(url, {binary: true, progress: true}, function (req) {
-    load_maps_from_mtz_buffer(viewer, req.response, labels);
+    var t0 = performance.now();
+    var mtz = Gemmi.readMtz(req.response);
+    load_maps_from_mtz_buffer(viewer, mtz, labels);
+    log_timing(t0, 'load_maps_from_mtz');
     if (callback) { callback(); }
   });
 }
 
-function set_pdb_and_mtz_dropzone(viewer/*:Viewer*/, zone/*:Object*/) {
+function set_pdb_and_mtz_dropzone(Gemmi/*:Object*/, viewer/*:Viewer*/,
+                                  zone/*:Object*/) {
   viewer.set_dropzone(zone, function (file) {
     if (/\.mtz$/.test(file.name)) {
       var reader = new FileReader();
       reader.onloadend = function (evt/*:any*/) {
         if (evt.target.readyState == 2) {
-          load_maps_from_mtz_buffer(viewer, evt.target.result);
+          var t0 = performance.now();
+          var mtz = Gemmi.readMtz(evt.target.result);
+          load_maps_from_mtz_buffer(viewer, mtz);
+          log_timing(t0, 'mtz -> maps');
           if (viewer.model_bags.length === 0 && viewer.map_bags.length <= 2) {
             viewer.recenter();
           }
